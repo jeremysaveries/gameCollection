8bcd0e10c235d2291faafc9643c34951a65ffe42
jeremy saveries <jeremysaveries2@gmail.com>
2024-06-09 22:43:19 +0200

    debut du html pour jeux classer par genre

---
+++ js/home.js
@@ -0,0 +1,12 @@
+//<div class="carousel-wrapper">
+  //      <button class="carousel-btn prev-btn">&#8249;</button>
+    //    <div class="carousel-container">
+          
+          
+      //    <div class="carousel-item">
+        //    <a href="#">
+          //    <img src="/public/img/jackette/game-fav/71d4d17edcd49703a5ea446cc0e588e6.jpg" alt="Image 2">
+            //</a>
+        //</div>
+
+   
