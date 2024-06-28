120bb3e9ab27b1fd80137a14dd4cb38b6f797297
jeremy saveries <jeremysaveries2@gmail.com>
2024-06-28 21:13:51 +0200

    feat: carousel
    test de carrousel avec bookstrap reste a le mettre dans le html index et regler les boutons et la vitesse de defilement
    
    carousel

---
+++ app/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MultipleItems from "./MultipleItems";  // Assurez-vous que le chemin est correct
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App</h1>
+      <MultipleItems />
+    </div>
+  );
+}
+
+export default App;
